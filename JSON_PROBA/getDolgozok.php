9c7a7b71a577690602581c390ecb8cc50513c0f4
Repits Mark <repitsmark@Repitss-MacBook-Pro.local>
2020-10-26 23:57:33 +0100

    small changes

--- JSON_PROBA/getDolgozok.php
+++ JSON_PROBA/getDolgozok.php
@@ -1,5 +1,5 @@
 <?php 
-	require 'connect.php';
+	require '../connect.php';
 	$sql = "SELECT d_nev, t_elnevezes, p_elnevezes, a_elnevezes FROM dolgozok, pozicio, allapot, terulet WHERE dolgozok.p_id = pozicio.p_id AND dolgozok.a_id = allapot.a_id AND dolgozok.t_id = terulet.t_id";
 	$result = $conn->query($sql);
 	$dolgozok = [];
