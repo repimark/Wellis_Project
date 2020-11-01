29c41c6673147343a1cf92b15517680009d27237
Repits Mark <repitsmark@Repitss-MacBook-Pro.local>
2020-11-01 19:27:16 +0100

    Index.php statsztika hozzáadva

--- getTerulet.php
+++ getTerulet.php
@@ -5,6 +5,33 @@ $result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		//echo $result->num_rows;
 		while($row = $result->fetch_assoc()) {
+			//SQL LEKÉRDEZÉSEK
+			$sqligenyMennyiseg = "SELECT SUM(i_db) FROM `igeny` WHERE t_id = '".$row["id"]."' AND i_sajat = '0'";
+			$sqlDolgozoMennyiseg = "SELECT COUNT(d_id) FROM `dolgozok` WHERE t_id = '".$row["id"]."' AND a_id = '1' OR a_id = '3'";
+			$sqligenyMennyisegKolcson = "SELECT SUM(i_db) FROM `igeny` WHERE t_id = '".$row["id"]."' AND i_sajat = '1'";
+			$sqlDolgozoMennyisegKolcson = "SELECT COUNT(d_id) FROM `dolgozok` WHERE t_id = '".$row["id"]."' AND a_id = '4'";
+
+			//QUERY FUTTATÁSOK
+			$resultIgeny = $conn->query($sqligenyMennyiseg);
+			$resultDolgozok = $conn->query($sqlDolgozoMennyiseg);
+			$resultKolcsonzottDolgozok = $conn->query($sqlDolgozoMennyisegKolcson);
+			$resultKolcsonzottIgeny = $conn->query($sqligenyMennyisegKolcson);
+
+			//FETCH ROW FUTTATÁS
+			$igenyAdat = mysqli_fetch_row($resultIgeny);
+			$dolgozAdat = mysqli_fetch_row($resultDolgozok);
+			$kolcsonzottAdat = mysqli_fetch_row($resultKolcsonzottDolgozok);
+			$kolcsonzottIgenyAdat = mysqli_fetch_row($resultKolcsonzottIgeny);
+
+			//ADATOK KINYERÉSE
+			$dolgozoMenny = (int) $dolgozAdat[0];
+			$igenyMenny = (int) $igenyAdat[0];
+			$kolcsonIgenyMenny = (int) $kolcsonzottIgenyAdat[0];
+			$kolcsonDolgozoMenny = (int) $kolcsonzottAdat[0];
+
+			//Összegek
+			$osszes = $dolgozoMenny + $igenyMenny;
+			$kolcsonOsszes = $kolcsonDolgozoMenny + $kolcsonIgenyMenny;
 ?>	
 	<div class="card mg-1 text-center " style="background: #E9967A; border: none;">
 			<div class="card-header bg-dribble" style="border:none; ">
@@ -14,54 +41,33 @@ $result = $conn->query($sql);
 				<div class="container">
 					<div class="row ">
 						<div class="col" >
-							<div class="progress mx-auto" data-value='25'>
-          						<span class="progress-left">
+							<div class="progress mx-auto" data-value='<?php echo ($dolgozoMenny/$osszes)*100; ?>'>
+          						<span class="progress-left" data-toggle="tooltip" data-placement="top" title="Igény Mennyiség: <?php echo $igenyMenny; ?>">
                         			<span class="progress-bar border-danger"></span>
           						</span>
-          						<span class="progress-right">
+          						<span class="progress-right" data-toggle="tooltip" data-placement="top" title="Dolgozó Mennyiség: <?php echo $dolgozoMenny; ?>">
                         			<span class="progress-bar border-danger"></span>
           						</span>
           					<div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
-            					<div class="h2 font-weight-bold">25<sup class="small">%</sup></div>
+            					<div class="h5 font-weight-bold"><?php echo $osszes." fő"; ?></div>
           					</div>
         					</div>
     					</div>
     				</div>
     				<div class="row p-2">
     					<div class="col">
-        					<div class="progress mx-auto" data-value='40'>
-          						<span class="progress-left">
+        					<div class="progress mx-auto" data-value='<?php echo ($kolcsonDolgozoMenny/$kolcsonOsszes)*100; ?>'>
+          						<span class="progress-left" data-toggle="tooltip" data-placement="top" title="Igény Mennyiség: <?php echo $kolcsonIgenyMenny; ?>">
                         			<span class="progress-bar border-danger"></span>
           						</span>
-          						<span class="progress-right">
-                        			<span class="progress-bar border-danger"></span>
+          						<span class="progress-right" data-toggle="tooltip" data-placement="top" title="Dolgozó Mennyiség: <?php echo $kolcsonDolgozoMenny; ?>">
+                        			<span class="progress-bar border-danger" ></span>
           						</span>
           						<div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
-            						<div class="h2 font-weight-bold">40<sup class="small">%</sup></div>
+            						<div class="h5 font-weight-bold"><?php echo $kolcsonOsszes." fő"; ?></div>
           						</div>
         					</div>
     					</div>
-
-						<!-- 				</div>
-						<div class="col ">
-							<p class="card-text">Jelenlegi fennmaradó igény <br> 13 fő</p>
-						</div>
-						</div>
-						<div class="row">
-						<div class="col ">
-							<p class="card-text">Kölcsönzött létszám <br> 5 fő</p>
-						</div>
-						<div class="col">
-							<p class="card-text">Kölcsönzött fennmaradó igény <br> 3 fő</p>
-						</div>
-						</div>
-						<div class="row">
-						<div class="col">
-							<p class="card-text">Összes létszám <br> 45 fő</p>
-						</div>
-						<div class="col ">
-							<p class="card-text">Összes fennmaradó igény <br> 16 fő </p>
-						</div> -->
 					</div>
 				</div>
 				<a class="btn text-monospace" data-toggle="collapse" data-target="#c<?php echo $row['id'];?>" aria-expanded="false" aria-controls="collapseExample" style="">Részletek</a>
@@ -69,7 +75,9 @@ $result = $conn->query($sql);
 			<div class="collapse" id="c<?php echo $row['id'];?>">
   				<div class="card card-body text-dark">
   					<?php if (vanMegjegyzes){ ?>
-  						<?php echo "van megjegyzes"; ?>
+  						<?php 
+  							
+  						?>
   					<?php }else{ ?>
   					<form>
   						<div class="form-group">
--- index.php
+++ index.php
@@ -22,6 +22,11 @@
       }
   	</style>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript">
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+    </script>
 </head>
 <body>
 
