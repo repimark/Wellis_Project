e1f3dc6b52d8d89651c06f2f0fdf49e47de3ba01
Repits Mark <repitsmark@Repitss-MacBook-Pro.local>
2021-01-12 01:06:05 +0100

    a

--- kileptetes.php
+++ kileptetes.php
@@ -9,7 +9,7 @@ $datum = $conn->real_escape_string($_POST["datum"]);
 $b_datum = $conn->real_escape_string($_POST["belepes"]);
 
 $sqlKilepInsert = "INSERT INTO `kilepett` (`k_id`, `d_nev`, `t_id`, `p_id`, `a_id`, `k_datum`, `b_datum`) VALUES (NULL, '" . $nev . "', '" . $t_id . "', '" . $p_id . "', '" . $a_id . "', '" . $datum . "', '" . $b_datum . "');";
-$sqlKilepDeleteMegjegyzes = "DELETE FROM `megjegyzes` WHERE `megjegyzes`.`d_id` = " . $id . ";";
+$sqlKilepDeleteMegjegyzes = "DELETE FROM `megjegyzes` WHERE `megjegyzes`.`d_id` = '" . $id . "';";
 $sqlKilepDelete = "DELETE FROM `dolgozok` WHERE `dolgozok`.`d_id` = '" . $id . "';";
 
 if ($conn->query($sqlKilepInsert)) {
