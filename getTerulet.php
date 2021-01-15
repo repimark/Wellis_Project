fac6d0f22ab311723846d8b8e77a7b5a5f89e327
Repits Mark <repitsmark@Repitss-MacBook-Pro.local>
2021-01-15 22:08:21 +0100

    szín

--- getTerulet.php
+++ getTerulet.php
@@ -90,7 +90,7 @@ $result = $conn->query($sql);
 								                       	backgroundColor: '#F5F5F5',
 								                        chartArea:{left:0,top:5,width:'100%',height:'90%'},
 								                   		
-								                   		colors: ['#33CC00','#ad62aa','#FFB300','#d6d2c4']
+								                   		colors: ['#33CC00','#ad62aa','#FFB300','#95a5a6']
 								                   		};
 
 								        // Instantiate and draw the chart for Sarah's pizza.
