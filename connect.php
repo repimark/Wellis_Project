cc488b814aef741b53561f4ae21c590dbaad3ac0
Repits Mark <repitsm@gmail.com>
2021-01-31 13:46:34 +0000

    .

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 // header('Content-Type: text/html; charset=utf-8');
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$db = 'alina_db';
+$host = 'localhost';
+$user = 'u227195896_repitsmark';
+$pass = 'Repka123.';
+$db = 'u227195896_alina_db';
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
