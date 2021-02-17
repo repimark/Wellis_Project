5579cf29ead4bde0b57e53fda00cd9705d02d155
Repits Mark <repitsmark@Repitss-MacBook-Pro.local>
2021-02-17 19:47:17 +0100

    .

--- contents/navbar.php
+++ contents/navbar.php
@@ -32,7 +32,7 @@
           <a class="dropdown-item" href="igenyValtozasok.php">Igény Változások</a>
           <a class="dropdown-item" href="kolcsonzok.php">Kölcsönzőcégek megoszlása</a>
           <a class="dropdown-item" href="haviDolgozok.php">Fluktuáció és belépés fő-ben (havi)</a>
-          <a class="dropdown-item" href="fluktuacio.php">Fluktuácó és belépés  %-ba (havi)</a>
+          <a class="dropdown-item" href="fluktuacio.php">Fluktuácó %-ba (havi)</a>
         </div>
       </li> 
     </ul>
